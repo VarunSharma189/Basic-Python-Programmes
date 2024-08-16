aa761af5cfe46b8f63a31393e670799d74ce405c
VarunSharma1890 <sharmav1890@gmail.com>
2024-08-17 01:27:14 +0530

    Comparison Operator

---
+++ Operators/Comparison_operators.py
@@ -0,0 +1,18 @@
+# Python program to demonstrate Comparison Operators
+a = int(input("Enter the value of a: "))
+b  = int(input("Enter the value of b: "))
+
+#equal to
+print(f"{a} == {b} : {a == b}")
+#not equal to
+print(f"{a} != {b} : {a != b}")
+#Greater than
+print(f"{a} > {b} : {a > b}")
+#less than
+print(f"{a} < {b} : {a < b}")
+#Greater than or equal to
+print(f"{a} >= {b} : {a >= b}")
+#Less than or equal to
+print(f"{a} <= {b} : {a <= b}")
+
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
