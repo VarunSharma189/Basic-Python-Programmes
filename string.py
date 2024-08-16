50039868268d4b3e7dcb2681bbe5c95f6d2a5229
Varun Sharma <sharmav1890@gmail.com>
2024-08-17 00:49:36 +0530

    Delete string.py

--- string.py
+++
@@ -1,9 +0,0 @@
-#Program to traverse a string in python
-my_string ="Bharati"
-pos =  int(input("Enter the position of the character to print: "))
-
-if 0 <= len(my_string):
-    print(f" The character at the specific string:",{my_string[pos]})
-else:
-    print("Invalid position")
-print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
