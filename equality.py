d3d057221fca7e59702c27386c98743e3ba3534b
VarunSharma1890 <sharmav1890@gmail.com>
2024-08-17 01:43:01 +0530

    Equality and Identity

---
+++ equality.py
@@ -0,0 +1,13 @@
+# Python program to demonstrate equality and identity
+
+a = b = [1, 2, 3]
+c = [1, 2, 3]
+
+print('id(a):', id(a), 'id(b):', id(b), 'id(c):', id(c)) # id of a and b is same, but c's is different
+
+print('a is b:', a is b) # output: True
+print('a is c:', a is c) # output: False
+
+# a and c are different objects
+
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
