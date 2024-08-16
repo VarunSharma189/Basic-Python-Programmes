4ebd28751e692a8002f4860709887275218bc293
VarunSharma1890 <sharmav1890@gmail.com>
2024-08-17 01:39:42 +0530

    Performing Math Functions

---
+++ Math_Functions.py
@@ -0,0 +1,12 @@
+#Python program to demonstate Basic Math Functions
+# Absolute value of a negative number
+absolute_value =  abs(-7) #7
+#rounding a number to a two decimal places
+rounded_value = round(3.14159 , 2) #3.14
+#Calculating power
+power_value = pow(2,3) #8
+
+print(f"absolute value : {absolute_value}")
+print(f"rounded value: {rounded_value}")
+print(f"power value : {power_value}")
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
