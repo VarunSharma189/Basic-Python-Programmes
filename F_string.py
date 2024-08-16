0d5ad322c9d27ef6321fa79a1313f90640d452d4
VarunSharma1890 <sharmav1890@gmail.com>
2024-08-17 01:44:37 +0530

    F_string

---
+++ F_string.py
@@ -0,0 +1,7 @@
+# Python program to demonstrate formatted or F string
+
+name = "Alice"
+age = 30
+print(f"Name: {name} , Age: {age}")
+
+print("THIS PROGRAM IS WRITTEN BY BARUN SHARMA ERP :- 0221BCA136")
