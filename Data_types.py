f9459a8c8980dbd887ea31b026bb0f9ef3adcd63
VarunSharma1890 <sharmav1890@gmail.com>
2024-08-17 00:44:18 +0530

    Data_Types

---
+++ Data_types.py
@@ -0,0 +1,37 @@
+# Python Program demonstrate various data type
+
+# Numbers
+integer = 42
+floating_point = 3.2
+complex_num= 1+2j
+
+#printing out
+print("Integer:" +str(integer))
+print("Floating_point:"+str(floating_point))
+print("Complex_num:"+str(complex_num))
+
+#Boolean
+testbool= True
+print("Boolean",testbool)
+#sequence
+string = "Hello world"
+tuple_seq =(1,2,3)
+list_seq = [1,2,3]
+
+print("String",string)
+print("Turple_seq",tuple_seq)
+print("List_seq",list_seq)
+
+#sets
+set_type={1,2,3}
+print("Set_type",set_type)
+
+#Mapping
+dictionary = {"key":"value"}
+print("dictionary",dictionary)
+
+#none
+none_type =""
+print("None", none_type)
+
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
