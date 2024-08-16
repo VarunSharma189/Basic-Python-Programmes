ea9aa4f2c993e949420caa8d8b66683c93b7d988
VarunSharma1890 <sharmav1890@gmail.com>
2024-08-17 01:29:20 +0530

    Membership Operator

---
+++ Operators/Membership_operator.py
@@ -0,0 +1,15 @@
+# Python program to demonstrate membership operator
+
+var1 = 'a' in 'abcd'  # True since 'a' is part of string "abcd"
+print(var1)
+
+var2 = 5 in [7, 6, 5, 4]  # True since 5 is in the list
+print(var2)
+
+var3 = 4 in [9, 0, 7, 8]  # False since 4 is not in the list
+print(var3)
+
+var4 = 'apple' in {'a': 'apple', 'b': 'boy', 'c': 'cat'}  
+print(var4)
+
+print("THIS PROGRAM IS WRITTEN BY BARUN SHARMA ERP :- 0221BCA136")
