e66598fe1af08b9bb1dfa521db621d11eb9a407c
VarunSharma1890 <sharmav1890@gmail.com>
2024-08-17 01:02:42 +0530

    Builtin Function

---
+++ Built_in_function.py
@@ -0,0 +1,12 @@
+#write a python program to demonstate any build in function
+user_input = input("Enter a list of numbers seperated by spaces")
+input_list = user_input.split()
+numbers = list(map(int, input_list))
+total = sum(numbers)
+length = len (numbers)
+sorted_numbers = sorted(numbers)
+print(f"Orignal list of numbers:{numbers}")
+print(f"Sum of numbers:{total}")
+print(f"Number of elements:{length}")
+print(f"Sorted list of numbers:{sorted_numbers}")
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
