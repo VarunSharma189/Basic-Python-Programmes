066f2ed2a2e7167572a3783cdfdbf6f764b88d88
VarunSharma1890 <sharmav1890@gmail.com>
2024-08-17 01:24:01 +0530

    Arithmetic Operator

---
+++ Operators/Arithmetic_operators.py
@@ -0,0 +1,22 @@
+# Python program to demonstrate Arithmetic Operators
+a=15
+b=4
+
+sum_result=a+b
+difference=a-b
+product=a*b
+quotient=a/b
+#FLOOR DIVISION AND MODULUS
+floor_div=a//b
+reminder=a%b
+#EXPONENTATION
+power=a**b
+#DISPLAY RESULTS
+print(f"sum: {sum_result}")
+print(f"difference: {difference}")
+print(f"product: {product}")
+print(f"quotient: {quotient}")
+print(f"floor_div: {floor_div}")
+print(f"reminder: {reminder}")
+print(f"power: {power}")
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
