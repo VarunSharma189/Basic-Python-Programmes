3ae7c580f94c8d6600d8cced0448728cb67ea37e
VarunSharma1890 <sharmav1890@gmail.com>
2024-08-17 01:25:07 +0530

    Assignment Operator, Bitwise Operator

---
+++ Operators/Assignement_operators.py
@@ -0,0 +1,25 @@
+# Python program to demonstrate Assignment Operators
+a = 10
+print(f"a = {a}")
+a += 15
+print(f"a +=  15 -> {a}")
+
+a-= 15
+print(f"a -= 15 -> {a}")
+
+a *= 15
+print(f"a *= 15 -> {a}")
+
+a /= 15
+print(f"a /= 15 -> {a}")
+
+a %= 15
+print(f"a %= 15 -> {a}")
+
+a //= 15
+print(f"a //= 15 -> {a}")
+
+a **= 15
+print(f"a **= 15 -> {a}")
+
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
