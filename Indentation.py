eecccfd308776914e1034cd0b882bd10ea882ab7
VarunSharma1890 <sharmav1890@gmail.com>
2024-08-16 22:40:47 +0530

    Indentation

---
+++ Indentation.py
@@ -0,0 +1,15 @@
+#proper indentation in a function
+def my_function():
+    print("this function is properly intended")
+    if True:
+        print("this is inside an if statement")
+# properly indented function
+def my_other_function():
+    print("this will not cause an error") #properly intended
+#properly indented in a loop
+for i in range(5):
+    print(i) #properly indented inside the loop
+#call the function to see the output
+my_function()
+my_other_function()
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
