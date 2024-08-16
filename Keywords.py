482de44181d87212f8cd082e4725bab34a5c4dbd
VarunSharma1890 <sharmav1890@gmail.com>
2024-08-16 22:33:02 +0530

    Create Keywords.py

---
+++ Keywords.py
@@ -0,0 +1,17 @@
+#using keywords in python
+#if-else conditional statements
+_variable=7
+if _variable > 5:
+    print("variable is greater than 5")
+else:
+    print("variable is 5 or less")
+#while loop
+count=0
+while count<5:
+    print(count)
+    count+=1
+
+# for loop
+for i in range(5):
+    print(i);
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
