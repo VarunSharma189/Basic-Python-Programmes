407b6c7d235606005ab30b43d08223ae83ec5e15
VarunSharma1890 <sharmav1890@gmail.com>
2024-08-17 01:29:03 +0530

    Logical Operator

---
+++ Operators/Logical_operators.py
@@ -0,0 +1,15 @@
+# Python program to demonstrate Logical Operators
+
+a = True
+b = True
+
+#AND Operator
+print(f"{a} and {b} : {a and b}")
+
+#OR Operator
+print(f"{a} or {b} : {a or b}")
+
+#NOT Operators
+print(f"not {a} :  {not a}")
+print(f"not {b} :  {not b}")
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
