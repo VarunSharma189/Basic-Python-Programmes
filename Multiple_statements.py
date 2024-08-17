03325a8347b7cfa906b8931922a87e57c9316c91
VarunSharma1890 <sharmav1890@gmail.com>
2024-08-17 10:19:41 +0530

    Multiple Statement

---
+++ Multiple_statements.py
@@ -0,0 +1,9 @@
+# Python program to combine multiple statements
+
+age = int(input("Enter your age: "))
+has_license = True
+
+if age >= 18 and has_license:
+    print("You can drive.")
+else:
+    print("You cannot drive.")  
