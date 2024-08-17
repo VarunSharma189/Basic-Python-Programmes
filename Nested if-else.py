9b35342734fa6e840fa5fb8e353d9f5d0e80c67c
VarunSharma1890 <sharmav1890@gmail.com>
2024-08-17 10:20:37 +0530

    Nested If-else

---
+++ Nested if-else.py
@@ -0,0 +1,14 @@
+# Python Program to demonstrate Nested If-else
+
+myAge = int(input('Enter age: '))
+if myAge >= 18:
+    myComment = "You can vote."
+else:
+    if myAge >= 10:
+        myComment = "You are in middle school."
+    else:
+        if myAge >= 6:
+            myComment = "You are in primary school."
+        else:
+            myComment = "You are too small to learn python"
+print("At age: " + str(myAge) + "->" + myComment)
