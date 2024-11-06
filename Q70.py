30911ce1b9290c31111ef7e936802fadc085cb04
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 01:29:13 +0530

    Iterate Through Dict. Items

---
+++ Q70.py
@@ -0,0 +1,6 @@
+# Program to Iterate Through Dictionary Items:
+
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+for key, value in my_dict.items():
+    print(f"{key}: {value}")
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
