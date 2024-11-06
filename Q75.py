1403bd39ef027c0c7f7ec2069b2b5fe405deb49e
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 01:31:40 +0530

    Conditional Returns

---
+++ Q75.py
@@ -0,0 +1,10 @@
+# Write a Python program to demonstrate Conditional returns
+
+def check_even_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+result = check_even_odd(4)
+print(result)
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
