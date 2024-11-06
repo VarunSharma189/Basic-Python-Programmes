aced05b307384fd303e5e6bf1670ca657b852728
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-06 23:53:32 +0530

    Q29.py

---
+++ Q29.py
@@ -0,0 +1,7 @@
+# Example of a while loop in Python
+count = 0
+
+while count < 5:
+    print("Count is:", count)
+    count += 1  
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
