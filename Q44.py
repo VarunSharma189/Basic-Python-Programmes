613bf07b5af1bb2527e03933937423c284d1c2c5
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 01:16:47 +0530

    Common Characters in string

---
+++ Q44.py
@@ -0,0 +1,13 @@
+# Write a python program to Print the Characters Which Are Common in Two Strings
+
+def common_characters(string_1, string_2):
+    for letter in string_1:
+        if letter in string_2:
+            print(f"Character '{letter}' is found in both the strings")
+
+def main():
+    common_characters('rose', 'goose')
+
+if __name__ == "__main__":
+    main()
+print("Written by Naman ERP :- 009")
