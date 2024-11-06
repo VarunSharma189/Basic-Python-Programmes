fdeff3e6d6debb244056da03241b724a525e4163
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 01:29:33 +0530

    Add Function takes 2 parameters

---
+++ Q71.py
@@ -0,0 +1,7 @@
+# Write a program to create add function that takes 2 parameters 
+
+def add(a,b):
+    return a+b
+result= add(10,20)
+print(result)
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
