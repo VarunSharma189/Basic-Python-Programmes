3f2b3be6abade348ddae2d74ef04cc2fe00646e7
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 01:32:10 +0530

    No Explicit Return

---
+++ Q77.py
@@ -0,0 +1,7 @@
+# Write a Python program to demonstrate No explicit return
+
+def greet(name):
+    print(f"Hello,{name}")
+result = greet("Varun")
+print(result)
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
