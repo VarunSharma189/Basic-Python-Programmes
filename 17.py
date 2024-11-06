16e56c5c5b2305fe8b8549ad24d734ac4e116886
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-06 23:37:40 +0530

    Q17

---
+++ 17.py
@@ -0,0 +1,22 @@
+a = 10
+b = 4
+
+#AND Operators
+print(f"{a} & {b} : {a & b} (binary:{bin(a & b)})")
+
+#OR Operator
+print(f"{a} | {b} : {a | b} (binary:{bin(a | b)})")
+
+#XOR Operator
+print(f"{a} ^ {b} : {a ^ b} (binary:{bin(a ^ b)})")
+
+#NOT Operator
+print(f" ~{a}  : ~{a } (binary:{bin(~a)})")
+
+#Left Shift Operator
+print(f" {a} << 1 : {a << 1} (binary:{bin(a<< 1)})")
+
+#Right Shift Operator
+print(f" {a} >> 1 : {a >> 1} (binary:{bin(a>> 1)})")
+
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
