b27f2e2b21b86e6f5c2b58cad0814828fa49e534
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-06 23:51:56 +0530

    Q28.py

---
+++ Q28.py
@@ -0,0 +1,9 @@
+#math module
+import math
+
+num = 16
+print(f"Square root of {num} is {math.sqrt(num)}")
+print(f"Factorial of {num} is {math.factorial(num)}")
+print(f"Logarithm of {num} is {math.log2(num)}")
+
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
