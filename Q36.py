ac304d5f299eebda184deb091477c68c0ed9752e
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 00:54:30 +0530

    Try and except

---
+++ Q36.py
@@ -0,0 +1,10 @@
+# write a python code to demonstrate try and except(Exception Handling)
+try:
+    number = int(input("Enter a number:"))
+    result = 10/number
+    print(result)
+except ZeroDivisionError:
+    print("Error : Cannot Divide by zero.")
+except ValueError:
+    print("Error:Invalid Input. Please enter a valid number.")
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
