eb5023129dd8fd9b06eac69e49629c571add94df
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 01:21:32 +0530

    Create Simple Generator

---
+++ Q53.py
@@ -0,0 +1,10 @@
+# Program to Create a Simple Generator:
+ 
+def count_up_to(max):
+   count = 1
+   while count <= max:
+      yield count
+      count += 1
+for num in count_up_to(10):
+ print(num) # Outputs: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+print("Written by Naman ERP :- 009")
