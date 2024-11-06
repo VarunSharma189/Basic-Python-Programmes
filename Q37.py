dca03e124a2fdf7e2db61826670cd8b8d9279ac8
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 01:10:46 +0530

    Try, except and else

---
+++ Q37.py
@@ -0,0 +1,11 @@
+# Write a python program to demonstrate try, except and else
+try:
+    number = int(input("Enter a number:"))
+    result = 10/number
+except ZeroDivisionError:
+    print("Error : Cannot Divide by zero.")
+except ValueError:
+    print("Error:Invalid Input. Please enter a valid number.")
+else:
+    print(f"The Result is {result}")    
+print("Written by Naman ERP :- 009")
