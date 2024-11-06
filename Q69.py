23b19e66f5369fd5b10fe8813db5e5e95ade0c75
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 01:28:49 +0530

    Access & Remove Dict Elements

---
+++ Q69.py
@@ -0,0 +1,8 @@
+# Program to Access and Remove Dictionary Elements:
+
+my_dict = {'name': 'Bob', 'age': 30, 'job': 'Engineer'}
+name = my_dict.get('name')
+my_dict.pop('job')
+print(name) # Outputs: Bob
+print(my_dict) # Outputs: {'name': 'Bob', 'age': 30}
+print("Written by Naman ERP :- 009")
