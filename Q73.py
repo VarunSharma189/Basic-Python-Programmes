67a02d06b77cc77b4c7f083a911a216111614c92
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 01:30:12 +0530

    Multiple Return Using Function

---
+++ Q73.py
@@ -0,0 +1,10 @@
+# Write a program to demonstrate multiple return using function
+
+def get_user_info():
+    name = "Alice"
+    age = 30
+    return name,age
+user_name , user_age = get_user_info()
+print(user_name)
+print(user_age)
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
