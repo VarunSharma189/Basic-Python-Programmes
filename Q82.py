fb9cbb8978543f59981adf38b1723a0d6e3afe0d
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 01:33:54 +0530

    How Decorators work

---
+++ Q82.py
@@ -0,0 +1,15 @@
+#WAP in py to demonstare how decorators work
+
+def my_decorator(func):
+    def wrapper():
+        print("Something is happening before the function is called.")
+        func()
+        print("Something is happening after the function is called.")
+    return wrapper
+
+@my_decorator
+def say_hello():
+    print("Hello!")
+
+say_hello()
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
