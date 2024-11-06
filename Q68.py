9b3bba60358427347b4b71aced49962914b674ec
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 01:28:27 +0530

    Create & Modify Dictionary

---
+++ Q68.py
@@ -0,0 +1,7 @@
+# Program to Create and Modify a Dictionary:
+
+my_dict = {'name': 'Alice', 'age': 25}
+my_dict['age'] = 26
+my_dict['city'] = 'New York'
+print(my_dict) # Outputs: {'name': 'Alice', 'age': 26, 'city': 'New York'}
+print("Written by Naman ERP :- 009")
