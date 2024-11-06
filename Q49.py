c4f229f8b6d3ca0669fdd529baf2719b7fc40f07
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 01:20:00 +0530

    List Comprehensions

---
+++ Q49.py
@@ -0,0 +1,7 @@
+# Program to Use List Comprehensions:
+
+squares = [x**2 for x in range(10)]
+even_squares = [x**2 for x in range(10) if x % 2 == 0]
+print("Squares:", squares) # Outputs: [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+print("Even squares:", even_squares) # Outputs: [0, 4, 16, 36, 64]
+print("Written by Naman ERP :- 009")
