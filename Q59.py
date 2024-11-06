8fcfc35ae0f9a5add41f75ae972a81592126e148
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 01:23:45 +0530

    Generator with Yield

---
+++ Q59.py
@@ -0,0 +1,9 @@
+# Program to Use Generator with yield:
+
+def countdown(n):
+   while n > 0:
+     yield n
+     n -= 1
+for number in countdown(5):
+ print(number) # Outputs: 5, 4, 3, 2, 1
+print("Written by Naman ERP :- 009")
