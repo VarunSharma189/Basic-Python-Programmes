32f2890d6f0226eaf4b55f9b4c888dcf78119099
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 01:23:29 +0530

    Lambda Expression with Map()

---
+++ Q58.py
@@ -0,0 +1,6 @@
+# Program to Use Lambda Expressions with map():
+
+numbers = [1, 2, 3, 4]
+squared = list(map(lambda x: x**2, numbers))
+print(squared) # Outputs: [1, 4, 9, 16]
+print("Written by Naman ERP :- 009")
