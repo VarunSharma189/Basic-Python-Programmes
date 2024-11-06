0fee4a282579bde9703b2163fba267367db002b6
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 01:15:57 +0530

    String Traversing

---
+++ Q42.py
@@ -0,0 +1,13 @@
+# Write a python program to demonstrate String Traversing using the for loop
+
+def main():
+    alphabet = "google"
+    index = 0
+    print(f"In the string '{alphabet}'")
+    for each_character in alphabet:
+        print(f"Character '{each_character}' has an index value of {index}")
+        index += 1
+
+if __name__ == "__main__":
+    main()
+print("Written by Naman ERP :- 009")
