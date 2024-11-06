232a5d6b6dd9c60e98901a6371b0d5194141ec7b
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 01:19:40 +0530

    Slice & Access List Ellements

---
+++ Q48.py
@@ -0,0 +1,10 @@
+# Program to Slice and Access List Elements:
+
+my_list = ['a', 'b', 'c', 'd', 'e']
+first_element = my_list[0]
+last_element = my_list[-1]
+sub_list = my_list[1:4]
+print("First element:", first_element) # Outputs: a
+print("Last element:", last_element) # Outputs: e
+print("Sliced list:", sub_list) # Outputs: ['b', 'c', 'd']
+print("Written by Naman ERP :- 009")
