20f9b4bad0d5446531ee4e8dae0797b295803eb8
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 01:30:56 +0530

    Return list through Function

---
+++ Q74.py
@@ -0,0 +1,9 @@
+# Write a program to return list through function
+
+def get_squares(numbers):
+    squares = [n**2 for n in numbers]
+    return squares
+nums = [1,2,3,4]
+squares_list = get_squares(nums)
+print(squares_list)
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
