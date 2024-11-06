d388dbeae4d0f9635923f36c7f64c7b203a61e03
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 01:28:05 +0530

    Perform Set Operations

---
+++ Q67.py
@@ -0,0 +1,11 @@
+# Program to Perform Set Operations:
+
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+union = set1 | set2
+intersection = set1 & set2
+difference = set1 - set2
+print("Union:", union) # Outputs: {1, 2, 3, 4, 5}
+print("Intersection:", intersection) # Outputs: {3}
+print("Difference:", difference) # Outputs: {1, 2}
+print("Written by Naman ERP :- 009")
