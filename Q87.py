cdfd4a072c7a47c59306a065285a895ee99576c7
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 01:34:56 +0530

    Aliasing Module & Functions

---
+++ Q87.py
@@ -0,0 +1,6 @@
+# WAP to demonstrate aliasing module and functions
+
+import math_utils as mu 
+result = mu.add(5, 45) 
+print(result) 
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
