0d6e88aa2ff6bf0d6b897aac4f5b92ecbe9549a7
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 01:29:50 +0530

    Create Default Function

---
+++ Q72.py
@@ -0,0 +1,7 @@
+# Write a program to create a default function 
+
+def greet(name="guest"):
+    print(f"Hello,{name}!")
+greet()
+greet("Varun")
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
