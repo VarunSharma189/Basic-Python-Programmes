e36b72da7e2111678fde1ed9fe3e6e670675cee8
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 01:22:35 +0530

    List Comprehension with Condition

---
+++ Q56.py
@@ -0,0 +1,6 @@
+# Program to Use List Comprehension with Condition:
+
+numbers = [1, 2, 3, 4, 5]
+even_numbers = [x for x in numbers if x % 2 == 0]
+print(even_numbers) # Outputs: [2, 4]
+print("Written by Naman ERP :- 009")
