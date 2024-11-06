43d9c4be123bccfce2c5c729369a948763a058e3
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 01:19:15 +0530

    Create & Modify a List

---
+++ Q47.py
@@ -0,0 +1,8 @@
+# Program to Create and Modify a List:
+
+my_list = [1, 2, 3, 4, 5]
+my_list.append(6)
+my_list.insert(2, 2.5)
+my_list.remove(4)
+print(my_list) # Outputs: [1, 2, 2.5, 3, 5, 6]
+print("Written by Naman ERP :- 009")
