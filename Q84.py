a8377151853f5cd99d93b91de23bd2222d20e0e0
VarunSharma1890 <sharmav1890@gmail.com>
2024-11-07 01:34:11 +0530

    Using a Module

---
+++ Q84.py
@@ -0,0 +1,11 @@
+# WAP to Using a Module
+import math_utils
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+result = math_utils.add(12, 3)
+print(result) 
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
