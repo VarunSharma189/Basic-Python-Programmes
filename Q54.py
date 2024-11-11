31f1a8baec6487b07bc244fe4c9eb6d3f034a601
Varun Sharma <sharmav1890@gmail.com>
2024-11-11 09:52:43 +0530

    Update Q54.py

--- Q54.py
+++ Q54.py
@@ -7,4 +7,4 @@ def fibonacci(n):
       a, b = b, a + b
 for num in fibonacci(10):
  print(num) # Outputs: 0, 1, 1, 2, 3, 5, 8
-print("Written by Naman ERP :- 009")
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
