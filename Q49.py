ae9bb68872ec6a23c5ebc6cd05e8a358315f4060
Varun Sharma <sharmav1890@gmail.com>
2024-11-11 09:50:42 +0530

    Update Q49.py

--- Q49.py
+++ Q49.py
@@ -4,4 +4,4 @@ squares = [x**2 for x in range(10)]
 even_squares = [x**2 for x in range(10) if x % 2 == 0]
 print("Squares:", squares) # Outputs: [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 print("Even squares:", even_squares) # Outputs: [0, 4, 16, 36, 64]
-print("Written by Naman ERP :- 009")
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
