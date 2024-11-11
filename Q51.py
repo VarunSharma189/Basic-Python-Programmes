55379038dfd0ed19f18fd2c85b71eddca9775932
Varun Sharma <sharmav1890@gmail.com>
2024-11-11 09:51:56 +0530

    Update Q51.py

--- Q51.py
+++ Q51.py
@@ -4,4 +4,4 @@ my_list = [1, 2, 3, 4]
 it = iter(my_list)
 print(next(it)) # Outputs: 1
 print(next(it)) # Outputs: 2
-print("Written by Naman ERP :- 009")
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
