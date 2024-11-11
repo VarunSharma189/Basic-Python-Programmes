6c1eb518b84dd45cb08046e2e6e31a444c7e7947
Varun Sharma <sharmav1890@gmail.com>
2024-11-11 09:52:29 +0530

    Update Q53.py

--- Q53.py
+++ Q53.py
@@ -7,4 +7,4 @@ def count_up_to(max):
       count += 1
 for num in count_up_to(10):
  print(num) # Outputs: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-print("Written by Naman ERP :- 009")
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
