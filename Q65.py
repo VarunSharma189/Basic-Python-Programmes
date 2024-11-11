622da24394db57af87f3cf2f66e1b542b9ebd9f0
Varun Sharma <sharmav1890@gmail.com>
2024-11-11 09:55:37 +0530

    Update Q65.py

--- Q65.py
+++ Q65.py
@@ -3,4 +3,4 @@
 my_tuple = (1, 2, 3, 4)
 print(my_tuple[0]) # Outputs: 1
 print(my_tuple[-1]) # Outputs: 4
-print("Written by Naman ERP :- 009")
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
