65962ae525afc5d7992ec820ce27f7705305d623
Varun Sharma <sharmav1890@gmail.com>
2024-11-11 09:56:37 +0530

    Update Q68.py

--- Q68.py
+++ Q68.py
@@ -4,4 +4,4 @@ my_dict = {'name': 'Alice', 'age': 25}
 my_dict['age'] = 26
 my_dict['city'] = 'New York'
 print(my_dict) # Outputs: {'name': 'Alice', 'age': 26, 'city': 'New York'}
-print("Written by Naman ERP :- 009")
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
