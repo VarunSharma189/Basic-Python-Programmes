001d210e760f9fef3a82381138fe8c1c4e5a2f9e
Varun Sharma <sharmav1890@gmail.com>
2024-11-11 09:53:44 +0530

    Update Q58.py

--- Q58.py
+++ Q58.py
@@ -3,4 +3,4 @@
 numbers = [1, 2, 3, 4]
 squared = list(map(lambda x: x**2, numbers))
 print(squared) # Outputs: [1, 4, 9, 16]
-print("Written by Naman ERP :- 009")
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
