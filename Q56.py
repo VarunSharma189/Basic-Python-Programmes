9f938ed1936ed3986e84d322d039f526224bc4e0
Varun Sharma <sharmav1890@gmail.com>
2024-11-11 09:53:14 +0530

    Update Q56.py

--- Q56.py
+++ Q56.py
@@ -3,4 +3,4 @@
 numbers = [1, 2, 3, 4, 5]
 even_numbers = [x for x in numbers if x % 2 == 0]
 print(even_numbers) # Outputs: [2, 4]
-print("Written by Naman ERP :- 009")
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
