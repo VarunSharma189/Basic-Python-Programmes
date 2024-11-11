ec710aa6f6effaac087cd475d830ce24e476d268
Varun Sharma <sharmav1890@gmail.com>
2024-11-11 09:53:27 +0530

    Update Q57.py

--- Q57.py
+++ Q57.py
@@ -2,4 +2,4 @@
 
 squares = {x**2 for x in range(5)}
 print(squares) # Outputs: {0, 1, 4, 9, 16}
-print("Written by Naman ERP :- 009")
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
