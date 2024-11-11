a962bf77b2a42d28d95dac2dfd56bfc5cfbd362b
Varun Sharma <sharmav1890@gmail.com>
2024-11-11 09:50:15 +0530

    Update Q47.py

--- Q47.py
+++ Q47.py
@@ -5,4 +5,4 @@ my_list.append(6)
 my_list.insert(2, 2.5)
 my_list.remove(4)
 print(my_list) # Outputs: [1, 2, 2.5, 3, 5, 6]
-print("Written by Naman ERP :- 009")
+print("THIS PROGRAM IS WRITTEN BY VARUN SHARMA ERP :- 0221BCA136")
